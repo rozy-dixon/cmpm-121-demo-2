6274d72efd9686df425d13eb70556ef07c8e6b3a
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-19 20:51:07 +0000

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,13 @@ const APP_NAME = "Computer Think Pt2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+// ---------------------------------------------- DISPLAY
+
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.style.width = canvas.style.height = '256px'
+app.append(canvas)
