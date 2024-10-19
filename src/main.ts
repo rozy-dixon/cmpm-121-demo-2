16f634b8897e1bfcfd80b235e890477d7b732906
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-19 05:57:56 +0000

    Step 0 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Computer Think Pt2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
