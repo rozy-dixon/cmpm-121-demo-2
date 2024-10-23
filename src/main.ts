8793bcf446c11b56bed792586748eba09c445e4b
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-23 05:52:52 +0000

    Step 6: Multiple markers

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ document.title = APP_NAME;
 
 // things to remember:
 // - shift, opt, f to format
+// - ternary operator
 
-// to-do 
+// to-do
 // - PWA/manifest
 // undo and redo clear
 
@@ -27,16 +28,26 @@ interface Point {
     y: number;
 }
 
+let isThick: boolean = false;
+const markerWeight = 10;
+const pencilWeight = 2;
+
 // src = https://chat.brace.tools/s/d667c7d4-4bcc-45a0-9ba2-9fab4366a24f
 interface Line {
     display: () => void;
     drag: (x: number, y: number) => void;
+    thickness: boolean;
 }
 
-function drawLine(startX: number, startY: number): Line {
+function drawLine(startX: number, startY: number, thickness: boolean): Line {
     const points: Array<Point> = [{ x: startX, y: startY }];
 
     function display() {
+        (context as CanvasRenderingContext2D).lineWidth = thickness == true
+            ? markerWeight
+            : pencilWeight;
+        (context as CanvasRenderingContext2D).lineCap = "round";
+
         context?.beginPath();
         const { x, y } = points[0];
         context?.moveTo(x, y);
@@ -49,7 +60,7 @@ function drawLine(startX: number, startY: number): Line {
         points.push({ x, y });
     }
 
-    return { display, drag };
+    return { display, drag, thickness };
 }
 
 const pastEdits: Array<Line | undefined> = [];
@@ -61,12 +72,15 @@ interface Button {
     // src = https://chat.brace.tools/s/43dd6ec1-7db6-4588-a338-49af88ca2f4c
     text: string;
     action: () => void;
+    class: string;
 }
 
 const buttons: Array<Button> = [
-    { text: "clear", action: clear },
-    { text: "undo", action: undo },
-    { text: "redo", action: redo },
+    { text: "clear", action: clear, class: "clear" },
+    { text: "undo", action: undo, class: "undo" },
+    { text: "redo", action: redo, class: "redo" },
+    { text: "marker", action: thick, class: "thick" },
+    { text: "pencil", action: thin, class: "thin" },
 ];
 
 // ---------------------------------------------- DISPLAY
@@ -85,11 +99,11 @@ const context: CanvasRenderingContext2D | null = canvas.getContext("2d");
 
 canvas.addEventListener("mousedown", (e) => {
     if (!cursor.drawing) {
-        presentEdit = drawLine(e.offsetX, e.offsetY);
+        presentEdit = drawLine(e.offsetX, e.offsetY, isThick);
         pastEdits.push(presentEdit);
         futureEdits.splice(0, futureEdits.length);
         cursor.drawing = true;
-    
+
         canvas.dispatchEvent(new CustomEvent("drawing-changed"));
     }
 });
@@ -145,8 +159,23 @@ function redo() {
     }
 }
 
+function thick() {
+    isThick = true;
+    (document.getElementById("thick") as HTMLElement).style.outline =
+        "4px auto yellowgreen";
+    (document.getElementById("thin") as HTMLElement).style.outline = "none";
+}
+
+function thin() {
+    isThick = false;
+    (document.getElementById("thin") as HTMLElement).style.outline =
+        "4px auto yellowgreen";
+    (document.getElementById("thick") as HTMLElement).style.outline = "none";
+}
+
 buttons.forEach((element) => {
     const button = document.createElement("button");
+    button.id = element.class;
     button.innerHTML = element.text;
     button.addEventListener("click", element.action);
     buttonDiv.append(button);
