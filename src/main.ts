fe55c3470e0c95b91dd6db1b47fe106c7b146d24
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-10-23 16:37:08 +0000

    Step 8: Multiple stickers

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,11 @@ document.title = APP_NAME;
 // undo and redo clear
 // - pass vars into events
 // - destroy redo after clear
+// - make selected button more concise
 
 // ---------------------------------------------- INITS
+
+// INPUT AND PREVIEW
 
 interface Input {
     x: number;
@@ -29,7 +32,7 @@ interface Preview {
     draw: () => void;
 }
 
-function previewInput(x: number, y: number, thickness: boolean): Preview {
+function previewInput(x: number, y: number, thickness: boolean, emoji: string | undefined): Preview {
     function draw() {
         context?.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -37,19 +40,32 @@ function previewInput(x: number, y: number, thickness: boolean): Preview {
             line?.display();
         });
 
-        context?.beginPath();
-        (context as CanvasRenderingContext2D).arc(x, y, 15, 0, Math.PI * 2);
-        (context as CanvasRenderingContext2D).lineWidth = thickness == true
-            ? markerWeight : pencilWeight;
-        (context as CanvasRenderingContext2D).globalAlpha = 0.25;
-        context?.stroke();
-        context?.closePath();
+        if (stickerMode && emoji != undefined) {
+            (context as CanvasRenderingContext2D).globalAlpha = 0.25;
+            (context as CanvasRenderingContext2D).font = `${stickerSize}px Inter`;
+            (context as CanvasRenderingContext2D).fillText(
+                emoji,
+                x - (stickerSize / 2),
+                y + (stickerSize / 2),
+            );
+        } else {
+            context?.beginPath();
+            (context as CanvasRenderingContext2D).arc(x, y, 15, 0, Math.PI * 2);
+            (context as CanvasRenderingContext2D).lineWidth = thickness == true
+                ? markerWeight
+                : pencilWeight;
+            (context as CanvasRenderingContext2D).globalAlpha = 0.25;
+            context?.stroke();
+            context?.closePath();
+        }
     }
 
-    return { draw }
+    return { draw };
 }
 
 let inputPreview: Preview;
+
+// LINE
 
 interface Point {
     x: number;
@@ -73,7 +89,8 @@ function drawLine(startX: number, startY: number, thickness: boolean): Line {
     function display() {
         (context as CanvasRenderingContext2D).globalAlpha = 1;
         (context as CanvasRenderingContext2D).lineWidth = thickness == true
-            ? markerWeight : pencilWeight;
+            ? markerWeight
+            : pencilWeight;
         (context as CanvasRenderingContext2D).lineCap = "round";
 
         context?.beginPath();
@@ -91,10 +108,41 @@ function drawLine(startX: number, startY: number, thickness: boolean): Line {
     return { display, drag, thickness };
 }
 
-const pastEdits: Array<Line | undefined> = [];
-const futureEdits: Array<Line | undefined> = [];
+const pastEdits: Array<Line | Sticker | undefined> = [];
+const futureEdits: Array<Line | Sticker | undefined> = [];
 
-let presentEdit: Line | undefined = undefined;
+let presentEdit: Line | Sticker | undefined = undefined;
+
+// STICKER
+
+const stickerSize: number = 20;
+let stickerMode: boolean = false;
+let currentSticker: string | undefined = undefined;
+
+interface Sticker {
+    display: () => void;
+    drag: (x: number, y: number) => void;
+}
+
+function placeSticker(x: number, y: number, emoji: string): Sticker {
+    function display() {
+        (context as CanvasRenderingContext2D).globalAlpha = 1;
+        (context as CanvasRenderingContext2D).font = `${stickerSize}px Inter`;
+        (context as CanvasRenderingContext2D).fillText(
+            emoji,
+            x - (stickerSize / 2),
+            y + (stickerSize / 2),
+        );
+    }
+    function drag (finalX: number, finalY: number) {
+        x = finalX;
+        y = finalY;
+
+        display()
+    }
+
+    return { display, drag };
+}
 
 // ---------------------------------------------- DISPLAY
 
@@ -111,7 +159,14 @@ const context: CanvasRenderingContext2D | null = canvas.getContext("2d");
 // ---------------------------------------------- CANVAS BASE FUNCTIONALITY
 
 canvas.addEventListener("mousedown", (e) => {
-    if (!cursor.drawing) {
+    if (stickerMode && currentSticker != undefined) {
+        presentEdit = placeSticker(e.offsetX, e.offsetY, currentSticker);
+        pastEdits.push(presentEdit);
+        futureEdits.splice(0, futureEdits.length);
+        cursor.drawing = true;
+
+        canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+    } else if (!cursor.drawing) {
         presentEdit = drawLine(e.offsetX, e.offsetY, isThick);
         pastEdits.push(presentEdit);
         futureEdits.splice(0, futureEdits.length);
@@ -122,11 +177,17 @@ canvas.addEventListener("mousedown", (e) => {
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    inputPreview = previewInput(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top, isThick)
+    inputPreview = previewInput(
+        e.clientX - canvas.getBoundingClientRect().left,
+        e.clientY - canvas.getBoundingClientRect().top,
+        isThick,
+        currentSticker,
+    );
     canvas.dispatchEvent(new CustomEvent("tool-moved"));
-    
-    if (cursor.drawing && presentEdit) {
-        presentEdit.drag(e.offsetX, e.offsetY);
+
+    if (presentEdit && cursor.drawing) {
+        console.log('hi there');
+        (presentEdit as Line).drag(e.offsetX, e.offsetY);
 
         canvas.dispatchEvent(new CustomEvent("drawing-changed"));
     }
@@ -163,19 +224,25 @@ interface Button {
     // src = https://chat.brace.tools/s/43dd6ec1-7db6-4588-a338-49af88ca2f4c
     text: string;
     action: () => void;
-    class: string;
+    id: string;
 }
 
 const buttons: Array<Button> = [
-    { text: "clear", action: clear, class: "clear" },
-    { text: "undo", action: undo, class: "undo" },
-    { text: "redo", action: redo, class: "redo" },
-    { text: "marker", action: thick, class: "thick" },
-    { text: "pencil", action: thin, class: "thin" },
+    { text: "clear", action: clear, id: "clear" },
+    { text: "undo", action: undo, id: "undo" },
+    { text: "redo", action: redo, id: "redo" },
+    { text: "marker", action: thick, id: "thick" },
+    { text: "pencil", action: thin, id: "thin" },
+    { text: "🪿", action: emoji, id: "emoji" },
+    { text: "🦢", action: emoji, id: "emoji" },
+    { text: "🕊️", action: emoji, id: "emoji" },
 ];
 
 const buttonDiv = document.createElement("div");
 app.append(buttonDiv);
+
+const emojiDiv = document.createElement("div");
+app.append(emojiDiv);
 
 function clear() {
     pastEdits.splice(0, pastEdits.length);
@@ -197,6 +264,7 @@ function redo() {
 }
 
 function thick() {
+    stickerMode = false;
     isThick = true;
     (document.getElementById("thick") as HTMLElement).style.outline =
         "4px auto yellowgreen";
@@ -204,16 +272,32 @@ function thick() {
 }
 
 function thin() {
+    stickerMode = false;
     isThick = false;
     (document.getElementById("thin") as HTMLElement).style.outline =
         "4px auto yellowgreen";
     (document.getElementById("thick") as HTMLElement).style.outline = "none";
 }
 
+function emoji() {
+    stickerMode = true;
+    (document.getElementById("thick") as HTMLElement).style.outline = "none";
+    (document.getElementById("thin") as HTMLElement).style.outline = "none";
+}
+
 buttons.forEach((element) => {
     const button = document.createElement("button");
-    button.id = element.class;
-    button.innerHTML = element.text;
-    button.addEventListener("click", element.action);
-    buttonDiv.append(button);
+    if (element.id == "emoji") {
+        button.innerHTML = element.text;
+        button.addEventListener("click", element.action);
+        button.addEventListener("click", function () {
+            currentSticker = element.text;
+        });
+        emojiDiv.append(button);
+    } else {
+        button.id = element.id;
+        button.innerHTML = element.text;
+        button.addEventListener("click", element.action);
+        buttonDiv.append(button);
+    }
 });
